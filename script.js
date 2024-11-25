050ae65619116470e859e77f16644750c9770759
KavyaNutubilli <141349522+KavyaNutubilli@users.noreply.github.com>
2024-11-25 23:19:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var t = document.getElementById("text");
+
+function redFont(){
+    t.style.color = "red";
+}
+
+function darkblueFont(){
+    t.style.color = "darkblue";
+}
+
+function greenFont(){
+    t.style.color = "green";
+}
+
+function blueFont(){
+    t.style.color = "blue";
+}
+
+function mediumvioletredFont(){
+    t.style.color = "mediumvioletred";
+}
+
+function deeppinkFont(){
+    t.style.color = "deeppink";
+}
+
+function indigoFont(){
+    t.style.color = "indigo";
+}
+
+function maroonFont(){
+    t.style.color = "maroon";
+}
+
+function orangeredFont(){
+    t.style.color = "orangered";
+}
+
+function purpleFont(){
+    t.style.color = "purple";
+}
+
+
+function aliceblueBg(){
+    t.style.backgroundColor = "aliceblue";
+}
+
+function ghostwhiteBg(){
+    t.style.backgroundColor = "ghostwhite";
+}
+
+function lavenderBg(){
+    t.style.backgroundColor = "lavender";
+}
+
+function lightcyanBg(){
+    t.style.backgroundColor = "lightcyan";
+}
+
+function mintcreamBg(){
+    t.style.backgroundColor = "mintcream";
+}
+
+function lavenderblushBg(){
+    t.style.backgroundColor = "lavenderblush";
+}
+
+function oldlaceBg(){
+    t.style.backgroundColor = "oldlace";
+}
+
+function mistyroseBg(){
+    t.style.backgroundColor = "mistyrose";
+}
+
+function thistleBg(){
+    t.style.backgroundColor = "thistle";
+}
+
+function seashellBg(){
+    t.style.backgroundColor = "seashell";
+}
+
+
+function cursive(){
+    t.style.fontFamily = "cursive";
+}
+
+function monospace(){
+    t.style.fontFamily = "monospace";
+}
+
+function serif(){
+    t.style.fontFamily = "serif";
+}
+
+function fantasy(){
+    t.style.fontFamily = "fantasy";
+}
+
+function sansSerif(){
+    t.style.fontFamily = "sans-serif";
+}
